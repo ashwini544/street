8da20017d515314af4fb5f29721d3fa3a117f71c
ashwini544 <t220055@famt.ac.in>
2025-09-29 22:49:43 +0530

    Create server.cjs

---
+++ server.cjs
@@ -0,0 +1,29 @@
+const express = require("express");
+
+let on = 0, total = 0;
+
+const app = express();
+app.get("/:oon/:ttotal", first);
+app.get("/", client);
+app.listen(8080);
+
+function first(req, res, err){
+
+	on += req.params.oon
+	total += req.params.ttotal
+	let used = (on/total)*100
+
+	res.json({"Percentage on": used});
+}
+
+function client(req, res, err){
+	let page = "<!doctype html>" +
+"<html>" +
+"<head><title>Streetlight Usage Tracker</title></head>" +
+"<body>" +
+"<h1> Percentage of time when lights were on : " + (on / total)*100 + "</h1>" +
+"</body>" +
+"</html>";
+
+	res.status(200).end(page);
+}
